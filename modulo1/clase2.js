1943adc66e054790978ec2f3c408b0dd7da02b1b
Anderson Laverde <anderson.laverde@zohomail.com>
2022-04-28 19:17:46 -0500

    Carga de las 3 primeras clases

---
+++ modulo1/clase2.js
@@ -0,0 +1,53 @@
+// Esta es la variable de la edad de anderson
+var edadAnderson = 22
+// Para poder imprimir el valor de una variable necesitamos hacer console.log
+console.log(edadAnderson)
+
+// Tipos de Datos
+// Numerico (integer, number, int)
+var numero = 10
+var numero2 = 8888888888888888888
+var numero3 = 3.1416
+// Texto (string, text)
+var texto1 = 'hola como estas'
+var texto2 = 'Hola mundo'
+var texto3 = '10'
+// Booleanos (bool, boolean)
+var boolean = true // verdadero
+var boolean2 = false // false
+// Arreglos, listas (array, list)
+var lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var lista2 = ['Anderson', 'Christopher', 'Cristhian']
+var lista3 = [true, true, false]
+var lista4 = [true, 100, 'Hola']
+var listaVacia = []
+var listaDeVariables = [numero, texto1, boolean2]
+var elemento4 = lista1[3]
+
+// Constantes
+const pi = 3.1416
+pi = "Esta es mi constante pi"
+alert(pi)
+
+// Modificadores
+// Para numbers
+var numero4 = 100
+var copiaNumero4 = numero4 // COpia de una variable
+// numero4 = numero4 - 10
+numero4 = numero4 + pi // numero4 = 100 + 3.1416 = 103.1416
+numero4 = numero4 * 200 + 2 + 20 / 34
+numero4 = numero4 / 2
+
+// Texto
+var hola = "Hola"
+var como = "Como"
+var estas = "estas"
+var saludo = `${hola} ${como} ${estas}`
+alert(saludo)
+
+// Booleanos
+
+// Listas
+
+
+// Condicionales
---
+++ modulo1/clase3.js
@@ -0,0 +1,9 @@
+// Proyecto en GIthub
+// Tipos de datos nulos e indefinidos
+// Terminar modificadores de listas
+// Objetos en javascript
+// Comparadores (>, <, ===, !==, ! )
+// Operadores logicos (&& ||)
+
+// Condicionales
+
